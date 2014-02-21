f69c59b3628bd94835059a83a553b74893126ca5
Oscar Fonts <oscar.fonts@gmail.com>
2014-02-21 08:22:22 +0100

    First Commit. Ultra-thin SOS 2 Client (json binding)

---
+++ config.js
@@ -0,0 +1,7 @@
+var LIB_PATH = "../lib/";
+
+require.config({
+    paths: {
+        "jquery": LIB_PATH + "jquery-2.1.0.min"
+    }
+});
---
+++ modules/SOS.js
@@ -0,0 +1,100 @@
+/**
+ * @author Oscar Fonts <oscar.fonts@geomati.co>
+ */
+define(['XML'], function(XML) {
+
+	SOS = {
+	    _url: null,
+	    
+		setUrl: function(url) {
+			this._url = url;
+		},
+
+	    getCapabilities: function(callback) {
+	    	var request = {
+	    		request: "GetCapabilities",
+	    		sections: ["Contents"]
+	    	};
+	    	
+	        this._send(request, function(response) {
+	    		callback(response.contents);
+	    	});
+	    },
+
+	    describeSensor: function(procedure, callback) {
+
+	        var request = {
+	            request: "DescribeSensor",
+	            procedure: procedure,
+	            procedureDescriptionFormat: "http://www.opengis.net/sensorML/1.0.1"
+	        };
+	
+	        this._send(request, function(response) {
+	            // Convert the SensorML description to a JSON object
+	            var description = response.procedureDescription.description;
+	            var json = XML.read(description, true);
+	            callback(json.SensorML.member);
+	        });
+	    },
+	
+	    getFeatureOfInterest: function(procedure, callback) {
+
+	        var request = {
+	            request: "GetFeatureOfInterest",
+	            procedure: procedure
+	        };
+	
+	        this._send(request, function(response) {
+	            callback(response.featureOfInterest);
+	        });
+	    },
+	
+	    getObservation: function(offering, features, properties, callback) {
+	        var request = {
+	            "request": "GetObservation"
+	        };
+	
+	        if (offering) {
+	            request["offering"] = offering;
+	        }
+	
+	        if (features && features.length) {
+	            request["featureOfInterest"] = features;
+	        }
+	
+	        if (properties && properties.length) {
+	            request["observedProperty"] = properties;
+	        }
+	
+	        this._send(request, function(response) {
+	            callback(response.observations);
+	        });
+	    },
+	
+	    _send: function(request, callback) {
+	    	request.service = "SOS";
+	    	request.version = "2.0.0";
+	    	
+	        var xhr = new XMLHttpRequest();
+	
+	        xhr.onreadystatechange = function() {
+	            if (xhr.readyState == 4) {
+	                if (xhr.status == 200) {
+	                    var response = JSON.parse(xhr.responseText);
+	                    callback.call(this, response);
+	                } else {
+	                    // TODO better error handling
+	                    alert("Error accessing " + this._url);
+	                }
+	            }
+	        }.bind(this);
+	        
+	        xhr.open("POST", this._url, true);
+	        xhr.setRequestHeader("Content-Type", "application/json");
+	        xhr.setRequestHeader("Accept", "application/json");
+	        xhr.send(JSON.stringify(request));
+	    }
+	};
+	
+	return SOS;
+});
