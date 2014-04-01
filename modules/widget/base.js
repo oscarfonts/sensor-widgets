99a7b127dae0070b827e7c809ac0506430b64e18
Oscar Fonts <oscar.fonts@gmail.com>
2014-04-01 16:10:33 +0200

    Empty base widget - keep it around FWIW

--- modules/widget/base.js
+++ modules/widget/base.js
@@ -3,20 +3,5 @@
  */
 define(["require"], function(require) {
 	return {
-		setDefaults: function(object, defaults) {
-			for (var key in defaults) {
-				if (!object.hasOwnProperty(key)) {
-					object[key] = defaults[key];
-				}
-			}
-			return object;
-		},
-		loadCss: function(url) {
-			var link = document.createElement("link");
-			link.type = "text/css";
-			link.rel = "stylesheet";
-			link.href = require.toUrl(url);
-			document.getElementsByTagName("head")[0].appendChild(link);
-		}
 	};
 });
