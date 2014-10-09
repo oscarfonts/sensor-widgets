7b1a4ad4c7b45446cd51842da712ce8d9a30941d
Oscar Fonts <oscar.fonts@gmail.com>
2014-10-09 11:13:14 +0200

    Refactored table widget. Now indicates UoMs.

--- src/js/widget/table.js
+++ src/js/widget/table.js
@@ -1,112 +1,83 @@
 /**
  * @author Oscar Fonts <oscar.fonts@geomati.co>
  */
-define(['SOS', 'locale-date'], function(SOS, ld) {
+define(['sos-data-access', 'locale-date'], function(data_access, ld) {
     "use strict";
 
     var inputs = ["title", "service", "offering", "feature", "properties", "time_start", "time_end"];
-    var propertyNames = null;
+
+    var template = [
+        '<div class="table widget">',
+            '<h3></h3>',
+            '<div class="table-responsive"></div>',
+        '</div>'
+    ].join('');
 
     return {
         inputs: inputs,
-        init: function(config, renderTo) {
-            SOS.setUrl(config.service);
-            read();
 
-            function read() {
-                var properties = config.properties = isArray(config.properties) ? config.properties : JSON.parse(config.properties);
-                var time_range = (config.time_start && config.time_end) ? [config.time_start, config.time_end] : null;
-                SOS.getObservation(config.offering, [config.feature], properties, time_range, draw);
-            }
+        init: function(config, el) {
 
-            function isArray(obj) {
-                return Object.prototype.toString.call(obj) === '[object Array]';
-            }
+            // Render template
+            el.innerHTML = template;
+            el.querySelector("h3").innerHTML = config.title;
+            var table = el.querySelector(".table-responsive");
 
-            function draw(observations) {
-                function d(n) {
-                    return n < 10 ? "0" + n : "" + n;
-                }
+            // Setup SOS data access
+            var data = data_access(config, redraw);
+            data.read();
 
+            // Update view
+            function redraw(data) {
                 // Get tabular data from observations
-                var data = {};
-                for (var i in observations) {
-                    var obs = observations[i];
+                var measures = {};
+                var properties = {};
+                for (var i in data) {
+                    var measure = data[i];
 
-                    var time = new Date(obs.resultTime).getTime();
-                    var property = obs.observableProperty;
-                    var value = obs.result.hasOwnProperty("value") ? obs.result.value : obs.result;
-
-                    if (!data[time]) {
-                        data[time] = {};
+                    // Add value in a time-indexed "measures" object
+                    var time = measure.time.getTime();
+                    if (!measures[time]) {
+                        measures[time] = {};
                     }
-                    data[time][property] = value;
+                    measures[time][measure.property] = measure.value;
+
+                    // Add property to a "properties" object, including uom
+                    if (!properties[measure.property]) {
+                        properties[measure.property] = measure.uom;
+                    }
                 }
 
-                if (observations.length) {
-                    getPropertyNames(observations[0].procedure, data);
-                }
+                createTable(measures, properties);
             }
 
-            function getPropertyNames(procedure, data) {
-                SOS.describeSensor(procedure, function(description) {
-                    var properties = description.hasOwnProperty("ProcessModel") ? description.ProcessModel.outputs.OutputList.output : description.System.outputs.OutputList.output;
-                    properties = properties instanceof Array ? properties : [properties];
-                    var types = ["Quantity", "Count", "Boolean", "Category", "Text", "ObservableProperty"];
-                    propertyNames = {};
-
-                    function isInArray(value, array) {
-                        return array.indexOf(value) > -1;
-                    }
-
-                    for (var i in properties) {
-                        var property = properties[i];
-                        for (var j in types) {
-                            var type = types[j];
-                            if (property.hasOwnProperty(type)) {
-                                property.id = property[type].definition;
-                            }
-                        }
-                        if (isInArray(property.id, config.properties)) {
-                            propertyNames[property.id] = property.name;
-                        }
-                    }
-                    createGrid(data, propertyNames);
-                });
-            }
-
-            function createGrid(data, propertyNames) {
-                var table = '<div class="table-responsive">';
-                table += '<table class="table table-striped table-condensed table-hover table-bordered">';
-                table += '<tr>';
-                table += '<thead>';
-                table += '<th>Result Time</th>';
-                for (var i in config.properties) {
-                    table += '<th>' + propertyNames[config.properties[i]] + '</th>';
+            function createTable(measures, properties) {
+                var html = '<table class="table table-striped table-condensed table-hover table-bordered">';
+                html += '<thead>';
+                html += '<tr>';
+                html += '<th>Result Time</th>';
+                for (var name in properties) {
+                    var uom = properties[name];
+                    html += '<th>' + name + " (" + uom + ')</th>';
                 }
-                table += '</tr>';
-                table += '</thead>';
+                html += '</tr>';
+                html += '</thead>';
 
-                var times = Object.keys(data);
+                var times = Object.keys(measures);
                 times.sort().reverse();
-                for (i in times) {
+                for (var i in times) {
                     var time = times[i];
-                    var values = data[time];
-                    table += '<tr>';
-                    table += '<th class="time">' + ld.display(new Date(parseInt(time))) + '</th>';
-                    for (var j in config.properties) {
-                        table += '<td>' + values[config.properties[j]] + '</td>';
+                    var values = measures[time];
+                    html += '<tr>';
+                    html += '<th class="time">' + ld.display(new Date(parseInt(time))) + '</th>';
+                    for (var property in properties) {
+                        html += '<td>' + values[property] + '</td>';
                     }
-                    table += '</tr>';
+                    html += '</tr>';
                 }
-                table += '</table>';
-                table += '</div>';
-
-                // Add table contents to document
-                var title = config.title ? "<h3>" + config.title + "</h3>" : "";
-                renderTo.innerHTML = title + table;
+                html += '</table>';
+                table.innerHTML = html;
             }
         }
     };
-
 });
